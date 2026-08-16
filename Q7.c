53971e504b21b1f11310a24c6cb5e7c11cd1deb9
Leeza-goyal <leezagoyal5@gmail.com>
2026-08-16 19:46:29 +0530

    Question7

---
+++ Q7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a=20;
+int b=30;
+printf("original value of a is %d\n", a);
+printf("original value of b is %d\n", b);
+printf("after swaping\n");
+printf("new value of a is %d\n" , b);
+printf("new value of b is %d\n", a);
+return 0;
+
+}
