b83758fccb62f402f9f5c1600d50c2bfb7734ea3
Leeza-goyal <leezagoyal5@gmail.com>
2026-08-16 19:47:04 +0530

    Question8

---
+++ Q8.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int n ;
+printf("enter value of n upto which you need to calculate sum ");
+scanf("%d", &n);
+printf("sum of n natural numbers is : %d\n" , (n*(n+1))/2 );
+return 0 ;
+}
