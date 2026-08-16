1381e6bef983d87e97206e5297ffa9a568d9c5c3
Leeza-goyal <leezagoyal5@gmail.com>
2026-08-16 18:48:16 +0530

    Question 1

---
+++ Q1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b ;
+printf("enter value of a "); 
+scanf("%d", &a) ;
+printf("enter value of b"); 
+scanf("%d", &b);
+printf("sum of two numbers is %d\n", a+b ); 
+return 0;
+}
+
