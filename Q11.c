391c09bb4f929feef795a2609c00c2f11c999407
Leeza-goyal <leezagoyal5@gmail.com>
2026-08-17 20:43:37 +0530

    Question 11

---
+++ Q11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int number;
+printf("enter any number\n");
+scanf("%d", &number);
+if(number%2==0)
+{
+printf("the entered number is even\n");
+}
+else
+{printf("number is odd\n");
+}
+return 0 ;
+}
