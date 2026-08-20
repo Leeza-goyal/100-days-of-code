1467eeb7428a0be9b529676fd47874ae120194f0
Leeza-goyal <leezagoyal5@gmail.com>
2026-08-20 10:24:52 +0530

    Question20

--- Q20.c
+++ Q20.c
@@ -1,36 +1,25 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
 int day;
-printf("enter any number form 1-7\n");
+printf("enter number from 1-7\n");
 scanf("%d", &day);
-if(day==1)
-{
-printf("monday\n");
-}
-else if(day==2)
-{
-printf("tuesday\n");
-}
-else if(day==3)
-{
-printf(" wednesday\n");
-}
-else if(day==4)
-{
-printf("thursday\n");
-}
-else if(day==5)
-{
-printf(" friday\n");
-}
-else if(day==6)
-{
-printf(" saturday\n");
-}
-else
-{
-printf("sunday\n");
+switch(day){
+case 1 : printf("monday\n");
+break;
+case 2 : printf("tuesday\n");
+break;
+case 3 : printf("wednesday\n");
+break;
+case 4 : printf("thursday\n");
+break;
+case 5 : printf("friday\n");
+break;
+case 6 : printf("saturday\n");
+break;
+case 7 : printf("sunday\n");
+break;
 }
 return 0 ;
 }
