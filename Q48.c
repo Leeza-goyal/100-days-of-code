5bc50527dc07393768f2a5459a239ac90d95be91
Leeza-goyal <leezagoyal5@gmail.com>
2026-09-03 10:21:34 +0530

    Question 48

---
+++ Q48.c
@@ -0,0 +1,15 @@
+//write a program to print pattern 
+#include<stdio.h>
+int main()
+{
+int row,col;
+for(row=1;row<=5;row++)
+{
+for(col=1;col<=row;col++)
+{
+printf("%d", col);
+}
+printf("\n");
+}
+return 0 ;
+}
